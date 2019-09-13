ff1c1e0c055aab0fca0648cb6f2f327d6152a13a
show <Terry@Terrys-MacBook-Pro.local>
2019-09-13 17:39:10 +0800

    td icon

--- demo/sht/assets/js/common.js
+++ demo/sht/assets/js/common.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 	
 	// 左侧边栏宽窄切换开关
-	$(".sidebar .btn-menu").click(function(){
-    	$(".sidebar").toggleClass("slim");
-    	$(".main").toggleClass("wide");
+	$('.sidebar .btn-menu').click(function(){
+    	$('.sidebar').toggleClass('slim');
+    	$('.main').toggleClass('wide');
 	});
 	
 	// 菜单点击激活后的样式
-	$(".sidebar .nav li").not("li:first").click(function(){
-    	$(this).addClass("active").siblings().removeClass("active");
+	$('.sidebar .nav li').not('li:first').click(function(){
+    	$(this).addClass('active').siblings().removeClass('active');
 	});
 	
 	// 让最后三个下拉菜单向上打开
-    $(".sidebar .nav .dropdown").slice(-3).addClass("dropup");
+    $('.sidebar .nav .dropdown').slice(-3).addClass('dropup');
     
 	// dropdown多级下拉菜单
     $('.dropdown-submenu > a').submenupicker();
@@ -21,11 +21,11 @@ $(document).ready(function(){
 	$('.dropdown-toggle').bootstrapDropdownHover();
 
 	// checked-all
-	$(".table th .icheckbox :checkbox").click(function(){
+	$('.table th .icheckbox :checkbox').click(function(){
 		if(this.checked){ 
-       		$(this).parents(".table").find(":checkbox").prop("checked",true);
+       		$(this).parents('.table').find(':checkbox').prop('checked',true);
        	}else{
-	       	$(this).parents(".table").find(":checkbox").prop("checked",false);
+	       	$(this).parents('.table').find(':checkbox').prop('checked',false);
        	}
 	});
 	
